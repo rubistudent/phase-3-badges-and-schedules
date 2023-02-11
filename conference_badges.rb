612de1036fabefad2e1eb882184530fbbac580ac
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-12 02:00:34 +0300

    completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(attendees)
+    attendees.map { |name| badge_maker(name) }
+  end
+  
+  def assign_rooms(attendees)
+      attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  end
+  
+  def printer(attendees)
+    batch_badge_creator(attendees).each { |badge| puts badge }
+    assign_rooms(attendees).each { |assignment| puts assignment }
+  end
